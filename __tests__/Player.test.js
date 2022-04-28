94a3bcbc8802fdf4faad52173e790a0bd66db2f5
Sal <salvador.banuelos.424@gmail.com>
2022-04-28 11:42:18 -0700

    33

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -35,3 +35,19 @@ test('get inventory from player or returns false', () => {
 
     expect(player.getInventory()).toEqual(false);
 });
+
+test('gets players health value', () => {
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));    
+});
+
+test('checks if player is alive or not', () => {
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
